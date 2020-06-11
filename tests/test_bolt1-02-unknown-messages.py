b80320da4f65a7bf0f6834b4eb2c5a6ba2746408
Rusty Russell <rusty@rustcorp.com.au>
2020-06-11 16:19:25 +0930

    tests/test_bolt1-02-unknown-messages.py: new simple test.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ tests/test_bolt1-02-unknown-messages.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python3
+# Init exchange, with unknown messages
+#
+
+from lnprototest import (TryAll, Connect, ExpectMsg, Msg, RawMsg, ExpectError)
+import pyln.proto.message.bolt1
+from fixtures import *  # noqa: F401,F403
+
+
+def test_unknowns(runner, namespaceoverride):
+    # We override default namespace since we only need BOLT1
+    namespaceoverride(pyln.proto.message.bolt1.namespace)
+    test = [Connect(connprivkey='03'),
+            ExpectMsg('init'),
+            Msg('init', globalfeatures='', features=''),
+            TryAll([
+                [],
+                # BOLT #1:
+                # A receiving node:
+                #   - upon receiving a message of _odd_, unknown type:
+                #     - MUST ignore the received message.
+                [RawMsg(bytes.fromhex('270F'))],
+
+
+                # BOLT #1:
+                # A receiving node:...
+                #   - upon receiving a message of _even_, unknown type:
+                #     - MUST close the connection.
+                #     - MAY fail the channels.
+                [RawMsg(bytes.fromhex('2710')),
+                 ExpectError()]
+            ])]
+
+    runner.run(test)
