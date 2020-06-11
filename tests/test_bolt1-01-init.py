1d32f54fe44488b6fb5df7ad1c624fbf705d6d0b
Rusty Russell <rusty@rustcorp.com.au>
2020-06-11 14:58:01 +0930

    tests/test_bolt1-01-init.py: simply use lists instead of Sequence.
    This makes it more compact but no less readable.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tests/test_bolt1-01-init.py
+++ tests/test_bolt1-01-init.py
@@ -2,7 +2,7 @@
 # Variations on init exchange.
 # Spec: MUST respond to known feature bits as specified in [BOLT #9](09-features.md).
 
-from lnprototest import TryAll, Sequence, Connect, Disconnect, EventError, ExpectMsg, Msg, ExpectError
+from lnprototest import TryAll, Connect, Disconnect, EventError, ExpectMsg, Msg, ExpectError
 from pyln.proto.message import Message
 import pyln.proto.message.bolt1
 
@@ -28,49 +28,49 @@ def no_34_35(event, msg):
 
 def test_init(runner):
     ns = pyln.proto.message.bolt1.namespace
-    test = Sequence([Connect(connprivkey='03'),
-                     ExpectMsg(ns, Message(ns.get_msgtype('init'))),
-                     Msg(Message(ns.get_msgtype('init'), globalfeatures='', features='')),
-                     # optionally disconnect that first one
-                     TryAll([Sequence([]),
-                             Sequence([Disconnect()])]),
-                     Connect(connprivkey='02'),
-                     TryAll([
-                         # Even if we don't send anything, it should send init.
-                         Sequence([ExpectMsg(ns, Message(ns.get_msgtype('init')))]),
+    test = [Connect(connprivkey='03'),
+            ExpectMsg(ns, Message(ns.get_msgtype('init'))),
+            Msg(Message(ns.get_msgtype('init'), globalfeatures='', features='')),
 
-                         # Minimal possible init message.
-                         # Spec: MUST send `init` as the first Lightning message for any connection.
-                         Sequence([ExpectMsg(ns, Message(ns.get_msgtype('init'))),
-                                   Msg(Message(ns.get_msgtype('init'),
-                                               globalfeatures='', features=''))]),
+            # optionally disconnect that first one
+            TryAll([[], Disconnect()]),
 
-                         # SHOULD NOT set features greater than 13 in `globalfeatures`.
-                         Sequence([ExpectMsg(ns, Message(ns.get_msgtype('init')),
-                                             if_match=no_gf13),
-                                   # init msg with unknown odd global bit (19): no error
-                                   Msg(Message(ns.get_msgtype('init'),
-                                               globalfeatures='020000', features=''))]),
+            Connect(connprivkey='02'),
+            TryAll([
+                # Even if we don't send anything, it should send init.
+                [ExpectMsg(ns, Message(ns.get_msgtype('init')))],
 
-                         # Sanity check that bits 34 and 35 are not used!
-                         Sequence([ExpectMsg(ns, Message(ns.get_msgtype('init')), if_match=no_34_35),
-                                   # init msg with unknown odd local bit (19): no error
-                                   Msg(Message(ns.get_msgtype('init'),
-                                               globalfeatures='', features='020000'))]),
+                # Minimal possible init message.
+                # Spec: MUST send `init` as the first Lightning message for any connection.
+                [ExpectMsg(ns, Message(ns.get_msgtype('init'))),
+                 Msg(Message(ns.get_msgtype('init'),
+                             globalfeatures='', features=''))],
 
-                         # init msg with unknown even local bit (34): you will error
-                         Sequence([ExpectMsg(ns, Message(ns.get_msgtype('init'))),
-                                   Msg(Message(ns.get_msgtype('init'),
-                                               globalfeatures='', features='0100000000')),
-                                   ExpectError()]),
+                # SHOULD NOT set features greater than 13 in `globalfeatures`.
+                [ExpectMsg(ns, Message(ns.get_msgtype('init')), if_match=no_gf13),
+                 # init msg with unknown odd global bit (19): no error
+                 Msg(Message(ns.get_msgtype('init'),
+                             globalfeatures='020000', features=''))],
 
-                         # init msg with unknown even global bit (34): you will error
-                         Sequence([ExpectMsg(ns, Message(ns.get_msgtype('init'))),
-                                   Msg(Message(ns.get_msgtype('init'),
-                                               globalfeatures='0100000000', features='')),
-                                   ExpectError()]),
+                # Sanity check that bits 34 and 35 are not used!
+                [ExpectMsg(ns, Message(ns.get_msgtype('init')), if_match=no_34_35),
+                 # init msg with unknown odd local bit (19): no error
+                 Msg(Message(ns.get_msgtype('init'),
+                             globalfeatures='', features='020000'))],
 
-                         # FIXME: Test based on features of runner!
-                     ])])
+                # init msg with unknown even local bit (34): you will error
+                [ExpectMsg(ns, Message(ns.get_msgtype('init'))),
+                 Msg(Message(ns.get_msgtype('init'),
+                             globalfeatures='', features='0100000000')),
+                 ExpectError()],
+
+                # init msg with unknown even global bit (34): you will error
+                [ExpectMsg(ns, Message(ns.get_msgtype('init'))),
+                 Msg(Message(ns.get_msgtype('init'),
+                             globalfeatures='0100000000', features='')),
+                 ExpectError()],
+
+                # FIXME: Test based on features of runner!
+            ])]
 
     runner.run(test)
