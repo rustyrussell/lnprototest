577980dce7f43f8edb436376922c2b6f3898e061
niftynei <niftynei@gmail.com>
2021-04-09 15:10:01 -0500

    dual-funding: test for insufficiently paid for witnesses
    Should probably check that the channel is closed unilaterally too, now
    that I'm thinking about it but hmmm.

--- tests/test_bolt2-20-open_channel_accepter.py
+++ tests/test_bolt2-20-open_channel_accepter.py
@@ -2,9 +2,9 @@
 # Variations on open_channel
 
 from hashlib import sha256
-from lnprototest import TryAll, Connect, Block, FundChannel, ExpectMsg, Msg, RawMsg, KeySet, CreateFunding, Commit, Runner, remote_funding_pubkey, remote_revocation_basepoint, remote_payment_basepoint, remote_htlc_basepoint, remote_per_commitment_point, remote_delayed_payment_basepoint, Side, msat, remote_funding_privkey, regtest_hash, bitfield, Event, DualFundAccept, OneOf, CreateDualFunding, EventError, Funding, privkey_expand, AddInput, AddOutput, FinalizeFunding, AddWitnesses, dual_fund_csv
+from lnprototest import TryAll, Connect, Block, FundChannel, ExpectMsg, Msg, RawMsg, KeySet, CreateFunding, Commit, Runner, remote_funding_pubkey, remote_revocation_basepoint, remote_payment_basepoint, remote_htlc_basepoint, remote_per_commitment_point, remote_delayed_payment_basepoint, Side, msat, remote_funding_privkey, regtest_hash, bitfield, Event, DualFundAccept, OneOf, CreateDualFunding, EventError, Funding, privkey_expand, AddInput, AddOutput, FinalizeFunding, AddWitnesses, dual_fund_csv, ExpectError
 from lnprototest.stash import sent, rcvd, commitsig_to_send, commitsig_to_recv, funding_txid, funding_tx, funding, locking_script, get_member, witnesses
-from helpers import utxo, tx_spendable, funding_amount_for_utxo, pubkey_of, tx_out_for_index, privkey_for_index
+from helpers import utxo, tx_spendable, funding_amount_for_utxo, pubkey_of, tx_out_for_index, privkey_for_index, utxo_amount
 from typing import Any, Callable
 
 
@@ -880,7 +880,7 @@ def test_open_opener_with_inputs(runner: Runner, with_proposal: Any) -> None:
     runner.run(test)
 
 
-def test_dual_open_rbf(runner: Runner, with_proposal: Any) -> None:
+def test_df_accepter_opener_underpays_fees(runner: Runner, with_proposal: Any) -> None:
     with_proposal(dual_fund_csv)
 
     local_funding_privkey = '20'
@@ -891,19 +891,226 @@ def test_dual_open_rbf(runner: Runner, with_proposal: Any) -> None:
                           delayed_payment_base_secret='23',
                           shachain_seed='00' * 32)
 
+    # Index 6 is special, it's a 1of5 multisig script that
+    # only the test runner can spend
+    input_index = 6
+
+    # Since technically these can be sent in any order,
+    # we must specify this as ok!
+    expected_add_input = ExpectMsg('tx_add_input',
+                                   channel_id=rcvd('accept_channel2.channel_id'),
+                                   sequence=0xfffffffd,
+                                   script_sig='',
+                                   if_match=odd_serial)
+
+    funding_amount = 100000
+
     test = [Block(blockheight=102, txs=[tx_spendable]),
             Connect(connprivkey='02'),
             ExpectMsg('init'),
+
+            # BOLT-f53ca2301232db780843e894f55d95d512f297f9 #9:
+            # | 28/29 | `option_dual_fund`             | Use v2 of channel open, enables dual funding              | IN9      | `option_anchor_outputs`, `option_static_remotekey`   | [BOLT #2](02-peer-protocol.md)        |
             Msg('init', globalfeatures='', features=bitfield(12, 20, 29)),
-            FundChannel(amount=999877),
+
+            DualFundAccept(),
+
+            # Accepter side: we initiate a new channel.
+            Msg('open_channel2',
+                channel_id=channel_id_tmp(local_keyset, Side.local),
+                chain_hash=regtest_hash,
+                # Leave some room for a change output
+                funding_satoshis=funding_amount,
+                dust_limit_satoshis=546,
+                max_htlc_value_in_flight_msat=4294967295,
+                htlc_minimum_msat=0,
+                funding_feerate_perkw=1000,
+                commitment_feerate_perkw=253,
+                # We use 5, because c-lightning runner uses 6, so this is different.
+                to_self_delay=5,
+                max_accepted_htlcs=483,
+                locktime=100,
+                funding_pubkey=pubkey_of(local_funding_privkey),
+                revocation_basepoint=local_keyset.revocation_basepoint(),
+                payment_basepoint=local_keyset.payment_basepoint(),
+                delayed_payment_basepoint=local_keyset.delayed_payment_basepoint(),
+                htlc_basepoint=local_keyset.htlc_basepoint(),
+                first_per_commitment_point=local_keyset.per_commit_point(0),
+                channel_flags=1),
+
+            # Ignore unknown odd messages
+            TryAll([], RawMsg(bytes.fromhex('270F'))),
+
+            ExpectMsg('accept_channel2',
+                      channel_id=channel_id_v2(local_keyset),
+                      funding_satoshis=funding_amount,
+                      funding_pubkey=remote_funding_pubkey(),
+                      revocation_basepoint=remote_revocation_basepoint(),
+                      payment_basepoint=remote_payment_basepoint(),
+                      delayed_payment_basepoint=remote_delayed_payment_basepoint(),
+                      htlc_basepoint=remote_htlc_basepoint(),
+                      first_per_commitment_point=remote_per_commitment_point(0)),
+
+            # Create and stash Funding object and FundingTx
+            CreateDualFunding(fee=200,
+                              funding_sats=agreed_funding(Side.local),
+                              locktime=sent('open_channel2.locktime', int),
+                              local_node_privkey='02',
+                              local_funding_privkey=local_funding_privkey,
+                              remote_node_privkey=runner.get_node_privkey(),
+                              remote_funding_privkey=remote_funding_privkey()),
+
+            # Ignore unknown odd messages
+            TryAll([], RawMsg(bytes.fromhex('270F'))),
+
+            Msg('tx_add_input',
+                channel_id=rcvd('accept_channel2.channel_id'),
+                serial_id=0,
+                sequence=0xfffffffd,
+                prevtx=tx_spendable,
+                prevtx_vout=tx_out_for_index(input_index),
+                script_sig=''),
+
+            AddInput(funding=funding(),
+                     privkey=privkey_for_index(input_index),
+                     serial_id=sent('tx_add_input.serial_id', int),
+                     prevtx=sent(),
+                     prevtx_vout=sent('tx_add_input.prevtx_vout', int),
+                     script_sig=sent()),
+
+            expected_add_input,
+
+            AddInput(funding=funding(),
+                     serial_id=rcvd('tx_add_input.serial_id', int),
+                     prevtx=rcvd('tx_add_input.prevtx'),
+                     prevtx_vout=rcvd('tx_add_input.prevtx_vout', int),
+                     script_sig=rcvd('tx_add_input.script_sig')),
+
+            Msg('tx_add_output',
+                channel_id=rcvd('accept_channel2.channel_id'),
+                serial_id=0,
+                sats=agreed_funding(Side.local),
+                script=funding_lockscript(local_funding_privkey)),
+
+            AddOutput(funding=funding(),
+                      serial_id=sent('tx_add_output.serial_id', int),
+                      sats=sent('tx_add_output.sats', int),
+                      script=sent('tx_add_output.script')),
+
+            ExpectMsg('tx_add_output',
+                      channel_id=rcvd('accept_channel2.channel_id'),
+                      if_match=odd_serial),
+
+            AddOutput(funding=funding(),
+                      serial_id=rcvd('tx_add_output.serial_id', int),
+                      sats=rcvd('tx_add_output.sats', int),
+                      script=rcvd('tx_add_output.script')),
+
+            Msg('tx_add_output',
+                channel_id=rcvd('accept_channel2.channel_id'),
+                serial_id=2,
+                # This function is the key to this test.
+                # `change_amount` uses a P2WPKH weight to calculate
+                # the input's fees; for this test we're using the
+                # magic `index_input = 6`, which is a large
+                # P2WSH-multisig address.
+                sats=change_amount(Side.local, True,
+                                   '001473daa75958d5b2ddca87a6c279bb7cb307167037',
+                                   utxo_amount(input_index)),
+                script='001473daa75958d5b2ddca87a6c279bb7cb307167037'),
+
+            AddOutput(funding=funding(),
+                      serial_id=sent('tx_add_output.serial_id', int),
+                      script=sent(),
+                      sats=sent('tx_add_output.sats', int)),
+
+
+            FinalizeFunding(funding=funding()),
+
+            # Ignore unknown odd messages
+            TryAll([], RawMsg(bytes.fromhex('270F'))),
+
+            Msg('tx_complete',
+                channel_id=rcvd('accept_channel2.channel_id')),
+
+            ExpectMsg('tx_complete',
+                      channel_id=rcvd('accept_channel2.channel_id')),
+
+            Commit(funding=funding(),
+                   opener=Side.local,
+                   local_keyset=local_keyset,
+                   local_to_self_delay=rcvd('accept_channel2.to_self_delay', int),
+                   remote_to_self_delay=sent('open_channel2.to_self_delay', int),
+                   local_amount=msat(sent('open_channel2.funding_satoshis', int)),
+                   remote_amount=msat(rcvd('accept_channel2.funding_satoshis', int)),
+                   local_dust_limit=546,
+                   remote_dust_limit=546,
+                   feerate=253,
+                   local_features=sent('init.features'),
+                   remote_features=rcvd('init.features')),
+
+            # Ignore unknown odd messages
+            TryAll([], RawMsg(bytes.fromhex('270F'))),
+
+            Msg('commitment_signed',
+                channel_id=rcvd('accept_channel2.channel_id'),
+                signature=commitsig_to_send(),
+                htlc_signature='[]'),
+
+            # Ignore unknown odd messages
+            TryAll([], RawMsg(bytes.fromhex('270F'))),
+
+            ExpectMsg('commitment_signed',
+                      channel_id=rcvd('accept_channel2.channel_id'),
+                      signature=commitsig_to_recv()),
+
+            ExpectMsg('tx_signatures',
+                      channel_id=rcvd('accept_channel2.channel_id'),
+                      txid=funding_txid()),
+
+            Msg('tx_signatures',
+                channel_id=rcvd('accept_channel2.channel_id'),
+                txid=funding_txid(),
+                witness_stack=witnesses()),
+
+            ExpectError()
+            ]
+
+    runner.run(test)
+
+
+def test_df_opener_accepter_underpays_fees(runner: Runner, with_proposal: Any) -> None:
+    with_proposal(dual_fund_csv)
+
+    local_funding_privkey = '20'
+
+    local_keyset = KeySet(revocation_base_secret='21',
+                          payment_base_secret='22',
+                          htlc_base_secret='24',
+                          delayed_payment_base_secret='23',
+                          shachain_seed='00' * 32)
+
+    # Index 6 is special, only the test runner can spend it
+    # 6 is a 1 of 5 multisig, meant to test the witness fee calculations
+    input_index = 6
+    test = [Block(blockheight=102, txs=[tx_spendable]),
+            Connect(connprivkey='02'),
+            ExpectMsg('init'),
+
+            # BOLT-f53ca2301232db780843e894f55d95d512f297f9 #9:
+            # | 28/29 | `option_dual_fund`             | Use v2 of channel open, enables dual funding              | IN9      | `option_anchor_outputs`, `option_static_remotekey`   | [BOLT #2](02-peer-protocol.md)        |
+            Msg('init', globalfeatures='', features=bitfield(12, 20, 29)),
+
+            FundChannel(amount=900000, feerate=1000, expect_fail=True),
 
             ExpectMsg('open_channel2',
                       channel_id=channel_id_tmp(local_keyset, Side.remote),
                       chain_hash=regtest_hash,
-                      funding_satoshis=999877,
+                      funding_satoshis=900000,
                       dust_limit_satoshis=546,
                       htlc_minimum_msat=0,
                       to_self_delay=6,
+                      funding_feerate_perkw=1000,
                       funding_pubkey=remote_funding_pubkey(),
                       revocation_basepoint=remote_revocation_basepoint(),
                       payment_basepoint=remote_payment_basepoint(),
@@ -915,7 +1122,7 @@ def test_dual_open_rbf(runner: Runner, with_proposal: Any) -> None:
             Msg('accept_channel2',
                 channel_id=channel_id_v2(local_keyset),
                 dust_limit_satoshis=550,
-                funding_satoshis=0,
+                funding_satoshis=400000,
                 max_htlc_value_in_flight_msat=4294967295,
                 htlc_minimum_msat=0,
                 minimum_depth=3,
@@ -928,9 +1135,6 @@ def test_dual_open_rbf(runner: Runner, with_proposal: Any) -> None:
                 delayed_payment_basepoint=local_keyset.delayed_payment_basepoint(),
                 htlc_basepoint=local_keyset.htlc_basepoint(),
                 first_per_commitment_point=local_keyset.per_commit_point(0)),
-
-            # Ignore unknown odd messages
-            TryAll([], RawMsg(bytes.fromhex('270F'))),
 
             # Create and stash Funding object and FundingTx
             CreateDualFunding(fee=200,
@@ -954,17 +1158,40 @@ def test_dual_open_rbf(runner: Runner, with_proposal: Any) -> None:
                      prevtx_vout=rcvd('tx_add_input.prevtx_vout', int),
                      script_sig=rcvd('tx_add_input.script_sig')),
 
-            # Ignore unknown odd messages
-            TryAll([], RawMsg(bytes.fromhex('270F'))),
+            Msg('tx_add_input',
+                channel_id=sent('accept_channel2.channel_id'),
+                serial_id=1,
+                sequence=0xfffffffd,
+                prevtx=tx_spendable,
+                prevtx_vout=tx_out_for_index(input_index),
+                script_sig=''),
 
-            Msg('tx_complete',
-                channel_id=sent('accept_channel2.channel_id')),
+            AddInput(funding=funding(),
+                     privkey=privkey_for_index(input_index),
+                     serial_id=sent('tx_add_input.serial_id', int),
+                     prevtx=sent(),
+                     prevtx_vout=sent('tx_add_input.prevtx_vout', int),
+                     script_sig=sent()),
 
             # The funding output
             ExpectMsg('tx_add_output',
                       channel_id=sent('accept_channel2.channel_id'),
                       sats=agreed_funding(Side.remote),
                       if_match=even_serial),
+
+            Msg('tx_add_output',
+                channel_id=sent('accept_channel2.channel_id'),
+                serial_id=101,
+                # This function is the key to this test.
+                # `change_amount` uses a P2WPKH weight to calculate
+                # the input's fees; for this test we're using the
+                # magic `index_input = 6`, which is a large
+                # P2WSH-multisig address.
+                sats=change_amount(Side.remote, False,
+                                   '001473daa75958d5b2ddca87a6c279bb7cb307167037',
+                                   utxo_amount(input_index)),
+                script='001473daa75958d5b2ddca87a6c279bb7cb307167037'),
+
             # FIXME: They may send us the funding output second,
             # if there's also a change output
             AddOutput(funding=funding(),
@@ -972,7 +1199,10 @@ def test_dual_open_rbf(runner: Runner, with_proposal: Any) -> None:
                       sats=rcvd('tx_add_output.sats', int),
                       script=rcvd('tx_add_output.script')),
 
-            Msg('tx_complete', channel_id=sent('accept_channel2.channel_id')),
+            AddOutput(funding=funding(),
+                      serial_id=sent('tx_add_output.serial_id', int),
+                      script=sent(),
+                      sats=sent('tx_add_output.sats', int)),
 
             # Their change if they have one!
             OneOf([ExpectMsg('tx_add_output',
@@ -987,7 +1217,10 @@ def test_dual_open_rbf(runner: Runner, with_proposal: Any) -> None:
                              sats=rcvd('tx_add_output.sats', int),
                              script=rcvd('tx_add_output.script'))],
                   [ExpectMsg('tx_complete',
-                             channel_id=sent('accept_channel2.channel_id'))]),
+                             channel_id=sent('accept_channel2.channel_id')),
+                   Msg('tx_complete',
+                       channel_id=sent('accept_channel2.channel_id')),
+                   ]),
 
             FinalizeFunding(funding=funding()),
 
@@ -1004,15 +1237,9 @@ def test_dual_open_rbf(runner: Runner, with_proposal: Any) -> None:
                    local_features=sent('init.features'),
                    remote_features=rcvd('init.features')),
 
-            # Ignore unknown odd messages
-            TryAll([], RawMsg(bytes.fromhex('270F'))),
-
             ExpectMsg('commitment_signed',
                       channel_id=sent('accept_channel2.channel_id'),
                       signature=commitsig_to_recv()),
-
-            # Ignore unknown odd messages
-            TryAll([], RawMsg(bytes.fromhex('270F'))),
 
             Msg('commitment_signed',
                 channel_id=sent('accept_channel2.channel_id'),
@@ -1024,16 +1251,7 @@ def test_dual_open_rbf(runner: Runner, with_proposal: Any) -> None:
                 txid=funding_txid(),
                 witness_stack=witnesses()),
 
-            # Ignore unknown odd messages
-            TryAll([], RawMsg(bytes.fromhex('270F'))),
-
-            ExpectMsg('tx_signatures',
-                      channel_id=sent('accept_channel2.channel_id'),
-                      txid=funding_txid()),
-
-            AddWitnesses(funding=funding(),
-                         witness_stack=rcvd('witness_stack')),
-
+            ExpectError()
             ]
 
     runner.run(test)
