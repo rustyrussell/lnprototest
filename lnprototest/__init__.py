5330378e3841f0d1b04cda0c007d9745345a13ae
Rusty Russell <rusty@rustcorp.com.au>
2021-06-24 14:04:20 +0930

    proposals: allow proposals to add a new TLV field.
    We refuse to merge same-named TLVs across namespaces, so we want to
    create a new namespace from the merged TLVs.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lnprototest/__init__.py
+++ lnprototest/__init__.py
@@ -15,7 +15,7 @@ from .event import Event, Connect, Disconnect, Msg, RawMsg, ExpectMsg, MustNotMs
 from .structure import Sequence, OneOf, AnyOrder, TryAll
 from .runner import Runner, Conn, remote_revocation_basepoint, remote_payment_basepoint, remote_delayed_payment_basepoint, remote_htlc_basepoint, remote_per_commitment_point, remote_per_commitment_secret, remote_funding_pubkey, remote_funding_privkey
 from .dummyrunner import DummyRunner
-from .namespace import peer_message_namespace, namespace, assign_namespace
+from .namespace import peer_message_namespace, namespace, assign_namespace, make_namespace
 from .bitfield import bitfield, has_bit, bitfield_len
 from .signature import SigType, Sig
 from .keyset import KeySet
@@ -65,6 +65,7 @@ __all__ = [
     "peer_message_namespace",
     "namespace",
     "assign_namespace",
+    "make_namespace",
     "bitfield",
     "has_bit",
     "bitfield_len",
