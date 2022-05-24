ef9f62202df360e5dc990f8431ed244108ea6362
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-05-24 10:21:55 +0200

    ci: increase the iteration up to 20
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd lnprototest || exit
-for i in range{0..10};
+for i in range{0..20};
 do
   if make check PYTEST_ARGS='--runner=lnprototest.clightning.Runner -n4 --dist=loadfile --log-cli-level=DEBUG'; then
     echo "iteration $i succeeded"
