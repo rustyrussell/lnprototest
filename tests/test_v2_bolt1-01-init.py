738689ffa22539cd113c1ebf8fb3e691bc141137
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-07-22 19:46:19 +0200

    conn: Add the possibility to send and receive message through the connection
    This commit enable the connection to send a message through the wire in
    an ergonomic way.
    
    This feature is a basic blocks for the lnprototest refactoring that
    allow to semplify how to write test with lnprototest in the future
    by keeping the state with the peer by connection and keep inside
    the runner just the necessary logic to interact with the node.
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- tests/test_v2_bolt1-01-init.py
+++ tests/test_v2_bolt1-01-init.py
@@ -11,10 +11,10 @@ def test_v2_init_is_first_msg(runner: Runner, namespaceoverride: Any) -> None:
     """
     runner.start()
 
-    runner.connect(None, connprivkey="03")
-    init_msg = runner.recv_msg()
+    conn1 = runner.connect(None, connprivkey="03")
+    init_msg = conn1.recv_msg()
     assert (
         init_msg.messagetype.number == 16
     ), f"received not an init msg but: {init_msg.to_str()}"
-
+    conn1.send_msg("init", globalfeatures="", features="")
     runner.stop()
