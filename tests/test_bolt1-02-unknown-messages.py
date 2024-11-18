fb0efe2e29b4fa98a733b866388ca86587083007
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-11-18 16:21:13 +0100

    tests: split the test in two
    A system test should do a single thing at once otherwise
    it make the code hard to read.
    
    So this is doing it.
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- tests/test_bolt1-02-unknown-messages.py
+++ tests/test_bolt1-02-unknown-messages.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python3
 # Init exchange, with unknown messages
 #
+import pyln.spec.bolt1
+
+from typing import Any
 
 from lnprototest import TryAll, Connect, ExpectMsg, Msg, RawMsg, Runner
 from lnprototest.event import ExpectDisconnect
-import pyln.spec.bolt1
-from typing import Any
+from lnprototest.utils import run_runner
 
 
 def test_unknowns(runner: Runner, namespaceoverride: Any) -> None:
@@ -20,6 +22,17 @@ def test_unknowns(runner: Runner, namespaceoverride: Any) -> None:
         #   - upon receiving a message of _odd_, unknown type:
         #     - MUST ignore the received message.
         RawMsg(bytes.fromhex("270F")),
+    ]
+    run_runner(runner, test)
+
+
+def test_unknowns_even_message(runner: Runner, namespaceoverride: Any) -> None:
+    # We override default namespace since we only need BOLT1
+    namespaceoverride(pyln.spec.bolt1.namespace)
+    test = [
+        Connect(connprivkey="03"),
+        ExpectMsg("init"),
+        Msg("init", globalfeatures="", features=""),
         # BOLT #1:
         # A receiving node:...
         #   - upon receiving a message of _even_, unknown type:
@@ -28,5 +41,4 @@ def test_unknowns(runner: Runner, namespaceoverride: Any) -> None:
         RawMsg(bytes.fromhex("2710")),
         ExpectDisconnect(),
     ]
-
-    runner.run(test)
+    run_runner(runner, test)
