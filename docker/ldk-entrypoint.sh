1f9f247543adf088afcc8e02f6785145512bc34f
Psycho-Pirate <prakharrsaxena@gmail.com>
2025-03-21 21:59:57 +0530

    LDK workflow and dockerfile added
    This commit adds a workflow for integrating LDK-Sample with Lnprototest. A dockerfile is also added to build LDK-Sample and run Lnprototest against it.

--- docker/ldk-entrypoint.sh
+++ docker/ldk-entrypoint.sh
@@ -3,7 +3,7 @@ cd lnprototest || exit
 
 for i in range{0..5};
 do
-  if make check PYTEST_ARGS='--runner=lnprototest.clightning.Runner -n8 --dist=loadfile --log-cli-level=DEBUG'; then
+  if poetry run make check PYTEST_ARGS="--runner=ldk_lnprototest.Runner -n8 --dist=loadfile --log-cli-level=DEBUG"; then
     echo "iteration $i succeeded"
   else
     echo "iteration $i failed"
